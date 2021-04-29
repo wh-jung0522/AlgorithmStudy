b9b51fbdd174f38c88fda04e01567087de938157
whjung <kwdg1213@kaist.ac.kr>
2021-04-29 20:59:57 +0900

    [Heap3] init

---
+++ 3.Heap/3_BinQue.py
@@ -0,0 +1,14 @@
+import heapq
+def solution(operations):
+    ## TODO
+    # Step1. 명령어 Get
+    # Step2. Parsing
+    # Step3-1 "I num" -> heap.push(num)
+    # Step3-2 "D index" -> heap.pop(0 or -1)
+
+    answer = []
+    return answer
+
+if __name__ == '__main__':
+    print(solution(["I 16","D 1"]) [0,0])
+    print(solution(["I 7","I 5","I -5","D -1"])[7, 5])
