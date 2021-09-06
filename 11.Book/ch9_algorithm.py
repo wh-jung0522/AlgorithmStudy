97045416d0ea966af39299bb644480f126d3e277
wh-jung0522 <59599991+wh-jung0522@users.noreply.github.com>
2021-09-07 02:53:44 +0900

    Update ch9_algorithm.py

--- 11.Book/ch9_algorithm.py
+++ 11.Book/ch9_algorithm.py
@@ -1,10 +1,13 @@
+INF = 1e9
 class route_algorithm():
     def __init__(self, graph) -> None:
         self.graph = graph
         pass
-    def dijkstra(self,start_node:list):
-        x,y = start_node
-        
-        return 
-    def floyd():
+    def dijkstra(self,start_node):
+        shortest_list = [INF] * len(self.graph)
+        shortest_list[start_node] = 0
+        return
+    def new_dijkstra(self,start_node):
+        return
+    def floyd(self):
         return
